b89e54d526028561e2b35352db979499dfbdd1c1
aajuy <obj.property@gmail.com>
2023-09-11 00:42:25 +0900

    modify: Changed time measuring area

--- GomokuOnlineDummyClient/Program.cs
+++ GomokuOnlineDummyClient/Program.cs
@@ -15,6 +15,7 @@ namespace GomokuOnlineDummyClient
 {
     class Program
     {
+        static Stopwatch stopWatch = new Stopwatch();
         static JsonSerializerOptions options = new JsonSerializerOptions
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase
@@ -22,12 +23,7 @@ namespace GomokuOnlineDummyClient
 
         static void Main(string[] args)
         {
-            if (args.Length == 0)
-            {
-                return;
-            }
-
-            int iterations = Int32.Parse(args[0]);
+            int iterations = 10;
             Test(iterations).Wait();
         }
 
@@ -39,7 +35,11 @@ namespace GomokuOnlineDummyClient
             string password = guid.ToString().Substring(12, 12);
 
             // Create stopwatch
-            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            stopWatch.Stop();
+
+            // Start time
+            Console.WriteLine($"Start time: {DateTime.UtcNow}");
 
             // Register
             RegisterRequestDto registerRequestDto = new RegisterRequestDto()
@@ -47,10 +47,7 @@ namespace GomokuOnlineDummyClient
                 Username = username,
                 Password = password
             };
-            stopWatch.Start();
             await Register(registerRequestDto);
-            stopWatch.Stop();
-            Console.WriteLine($"Register:{stopWatch.ElapsedMilliseconds}");
 
             // Login
             LoginRequestDto loginRequestDto = new LoginRequestDto()
@@ -58,38 +55,24 @@ namespace GomokuOnlineDummyClient
                 Username = username,
                 Password = password
             };
-            stopWatch.Restart();
             LoginResponseDto loginResponseDto = await Login(loginRequestDto);
-            stopWatch.Stop();
-            Console.WriteLine($"Login:{stopWatch.ElapsedMilliseconds}");
             MyInfo.Instance.UserId = loginResponseDto.UserId;
             MyInfo.Instance.SessionId = loginResponseDto.SessionId;
             ServerConfig.MatchServerAddress = loginResponseDto.MatchServerAddress;
             ServerConfig.GameServerAddress = loginResponseDto.GameServerAddress;
 
             // Ranking
-            stopWatch.Restart();
             await Rankings();
-            stopWatch.Stop();
-            Console.WriteLine($"Ranking:{stopWatch.ElapsedMilliseconds}");
 
             // Stamina
-            stopWatch.Restart();
             await Stamina();
-            stopWatch.Stop();
-            Console.WriteLine($"Stamina:{stopWatch.ElapsedMilliseconds}");
 
             // Match / Game
             for (int i = 0; i < iterations; i++)
             {
-                stopWatch.Restart();
+                Console.WriteLine($"Match / Game {i}:");
                 Match();
-                stopWatch.Stop();
-                Console.WriteLine($"Match:{stopWatch.ElapsedMilliseconds}");
-                stopWatch.Restart();
                 Game();
-                stopWatch.Stop();
-                Console.WriteLine($"Game:{stopWatch.ElapsedMilliseconds}");
             }
 
             // Logout
@@ -98,10 +81,7 @@ namespace GomokuOnlineDummyClient
                 UserId = MyInfo.Instance.UserId,
                 SessionId = MyInfo.Instance.SessionId
             };
-            stopWatch.Restart();
             await Logout(logoutRequestDto);
-            stopWatch.Stop();
-            Console.WriteLine($"Logout:{stopWatch.ElapsedMilliseconds}");
         }
 
         static async Task Register(RegisterRequestDto registerRequestDto)
@@ -109,7 +89,10 @@ namespace GomokuOnlineDummyClient
             using (HttpClient client = new HttpClient())
             {
                 client.BaseAddress = new Uri(ServerConfig.LoginServerAddress);
+                stopWatch.Restart();
                 await client.PostAsJsonAsync("register", registerRequestDto);
+                stopWatch.Stop();
+                Console.WriteLine($"Register:{stopWatch.ElapsedMilliseconds}");
             }
         }
 
@@ -119,7 +102,10 @@ namespace GomokuOnlineDummyClient
             {
                 client.BaseAddress = new Uri(ServerConfig.LoginServerAddress);
                 HttpResponseMessage response = await client.PostAsJsonAsync("login", loginRequestDto, options);
+                stopWatch.Restart();
                 LoginResponseDto loginResponseDto = await response.Content.ReadFromJsonAsync<LoginResponseDto>(options);
+                stopWatch.Stop();
+                Console.WriteLine($"Login:{stopWatch.ElapsedMilliseconds}");
                 return loginResponseDto;
             }
         }
@@ -130,7 +116,10 @@ namespace GomokuOnlineDummyClient
             {
                 client.BaseAddress = new Uri("http://" + ServerConfig.MatchServerAddress);
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("SessionId", MyInfo.Instance.SessionId);
+                stopWatch.Restart();
                 await client.GetAsync($"stamina/{MyInfo.Instance.UserId}");
+                stopWatch.Stop();
+                Console.WriteLine($"Stamina:{stopWatch.ElapsedMilliseconds}");
             }
         }
 
@@ -139,7 +128,10 @@ namespace GomokuOnlineDummyClient
             using (HttpClient client = new HttpClient())
             {
                 client.BaseAddress = new Uri("http://" + ServerConfig.MatchServerAddress);
+                stopWatch.Restart();
                 HttpResponseMessage response = await client.GetAsync("ranking?from=1&to=99");
+                stopWatch.Stop();
+                Console.WriteLine($"Ranking:{stopWatch.ElapsedMilliseconds}");
                 RankingEntry[] rankingEntries = await response.Content.ReadFromJsonAsync<RankingEntry[]>(options);
             }
         }
@@ -149,6 +141,7 @@ namespace GomokuOnlineDummyClient
             IPAddress iPAddress = IPAddress.Parse(ServerConfig.MatchServerAddress);
             IPEndPoint endPoint = new IPEndPoint(iPAddress, 6789);
 
+            stopWatch.Restart();
             Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             socket.Connect(endPoint);
 
@@ -179,6 +172,8 @@ namespace GomokuOnlineDummyClient
                 action.Invoke(Session, packetMessage.packet);
                 if (Session.disconnected == 1)
                 {
+                    stopWatch.Stop();
+                    Console.WriteLine($"Match:{stopWatch.ElapsedMilliseconds}");
                     return;
                 }
             }
@@ -189,6 +184,7 @@ namespace GomokuOnlineDummyClient
             IPAddress iPAddress = IPAddress.Parse(ServerConfig.GameServerAddress);
             IPEndPoint endPoint = new IPEndPoint(iPAddress, 5678);
 
+            stopWatch.Restart();
             Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             socket.Connect(endPoint);
 
@@ -202,6 +198,8 @@ namespace GomokuOnlineDummyClient
                 Thread.Sleep(100);
                 if (Session.disconnected == 1)
                 {
+                    stopWatch.Stop();
+                    Console.WriteLine($"Game:{stopWatch.ElapsedMilliseconds}");
                     return;
                 }
 
@@ -220,6 +218,8 @@ namespace GomokuOnlineDummyClient
                 action.Invoke(Session, packetMessage.packet);
                 if (Session.disconnected == 1)
                 {
+                    stopWatch.Stop();
+                    Console.WriteLine($"Game:{stopWatch.ElapsedMilliseconds}");
                     return;
                 }
             }
@@ -230,7 +230,10 @@ namespace GomokuOnlineDummyClient
             using (HttpClient client = new HttpClient())
             {
                 client.BaseAddress = new Uri(ServerConfig.LoginServerAddress);
+                stopWatch.Restart();
                 await client.PostAsJsonAsync("logout", logoutRequestDto);
+                stopWatch.Stop();
+                Console.WriteLine($"Logout:{stopWatch.ElapsedMilliseconds}");
             }
         }
     }
